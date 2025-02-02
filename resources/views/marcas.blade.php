f31b3e557984272bce1c538bea7577cfe94182ae
Adolfo David Mauro <davidmauroweb@gmail.com>
2025-02-02 09:55:31 -0300

    crudMarcas

--- resources/views/marcas.blade.php
+++ resources/views/marcas.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Gestión de Categorías
+                <div class="card-header">Gestión de Marcas
                 </div>
                 <div class="card-body">
                     @if (session('status'))
